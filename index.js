c789137beef7b0b2feb29adaf2c03e77d1898734
majdaelabbady-code <majdaelabady@gmail.com>
2026-04-20 20:36:50 +0100

    modif master

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-"console.log('Hello Node');"  
+// VERSION MASTER
+function addition(a, b) {
+  return a + b;
+}
+
+// AUTRE MODIF ICI
+console.log("Master branch");
+
+if (require.main === module) {
+  console.log("Résultat:", addition(5, 3));
+}
+
+module.exports = { addition };
