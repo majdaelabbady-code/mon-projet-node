43156a95a554c11e3834326ea40ae0a83f3f3d32
majdaelabbady-code <majdaelabady@gmail.com>
2026-04-20 20:35:00 +0100

    Initialisation du projet Node (README, .gitignore, index.js)

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-function addition(a, b) {
-  return a + b;
-}
-
-function soustraction(a, b) {
-  return a - b;
-}
-
-if (require.main === module) {
-  console.log("Résultat addition:", addition(5, 3));
-  console.log("Résultat soustraction:", soustraction(5, 3));
-}
-
-module.exports = { addition, soustraction };
+"console.log('Hello Node');"  
