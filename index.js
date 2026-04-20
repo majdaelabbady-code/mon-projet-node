ab4c2f47d5ee4a97c4d8545d0747ce818324e6ff
majdaelabbady-code <majdaelabady@gmail.com>
2026-04-20 20:36:14 +0100

    modif feature

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function soustraction(a, b) {
   return a - b;
 }
 
+// MODIF ICI (ajoute un texte différent)
+console.log("Feature branch");
+
 if (require.main === module) {
   console.log("Résultat addition:", addition(5, 3));
   console.log("Résultat soustraction:", soustraction(5, 3));
