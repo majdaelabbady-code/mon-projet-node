410243957bcade4f0779b7d8b31da49917dc9b41
majdaelabbady-code <majdaelabady@gmail.com>
2026-04-21 17:18:46 +0100

    Ajout fonction soustraction

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function addition(a, b) {
-  const x = Number(a), y = Number(b);
-  if (Number.isNaN(x) || Number.isNaN(y)) return 0;
-  return x + y;
+    return a + b;
 }
 
 function soustraction(a, b) {
-  return a - b;
+    return a - b;
 }
 
 if (require.main === module) {
-  console.log("Résultat addition:", addition(5, 3));
-  console.log("Résultat soustraction:", soustraction(5, 3));
+    console.log("Addition:", addition(5, 3));
+    console.log("Soustraction:", soustraction(5, 3));
 }
 
 module.exports = { addition, soustraction };
